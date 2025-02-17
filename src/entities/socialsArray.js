46cf96a57b07c52bb23dbd60c27d535d8b2e9e3f
Arm1700 <bukboks1@gmail.com>
2025-02-17 15:31:06 +0400

    'correcct'

--- src/entities/socialsArray.js
+++ src/entities/socialsArray.js
@@ -1,11 +1,11 @@
-import {FaInstagram} from 'react-icons/fa6';
-import {AiOutlineFacebook} from "react-icons/ai";
-import {FaYoutubeSquare} from "react-icons/fa";
+import { FaInstagram } from 'react-icons/fa6';
+import { AiOutlineFacebook } from "react-icons/ai";
+import { FaYoutubeSquare } from "react-icons/fa";
 
 const socialsArray = [
-  {id: 1, name: 'facebook', Icon: AiOutlineFacebook,href:'https://www.facebook.com/'},
-  {id: 2, name: 'instagram', Icon: FaInstagram,href:'https://www.instagram.com/'},
-  {id: 3, name: 'youtube', Icon: FaYoutubeSquare,href:'https://www.youtube.com/'},
+  { id: 1, name: 'facebook', Icon: AiOutlineFacebook, href: 'https://www.facebook.com/share/1E9vSTXS4M/?mibextid=LQQJ4d' },
+  { id: 2, name: 'instagram', Icon: FaInstagram, href: 'https://www.instagram.com/grand_educational_center?igsh=dHJhOW5oZDVjb2t2' },
+  { id: 3, name: 'youtube', Icon: FaYoutubeSquare, href: 'https://www.youtube.com/' },
 ];
 
 export default socialsArray;
